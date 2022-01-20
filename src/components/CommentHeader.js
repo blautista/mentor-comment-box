2c5224c114342f85997cf24f4125918aa8d5a24a
blautista <bautistaromanazzi@gmail.com>
2022-01-20 20:32:35 -0300

    Added profile picture to CommentHeader

--- src/components/CommentHeader.js
+++ src/components/CommentHeader.js
@@ -4,7 +4,8 @@ import styles from './CommentHeader.module.css';
 const CommentHeader = (props) => {
   return (
     <div className={styles.container}>
-      <div>
+      <div className={styles.infoContainer}>
+        <img className={styles.profilePicture} alt="Profile picture" src={props.user.image.png}></img>
         <span className={styles.username}>{props.user.username}</span>
         <span className={styles.createdAt}>{props.createdAt}</span>
       </div>
