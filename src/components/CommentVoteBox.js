3bc3b03d32ffd63c75d2f7511a7e3745e978cc46
blautista <bautistaromanazzi@gmail.com>
2022-01-20 20:00:42 -0300

    added VoteBox styles

--- src/components/CommentVoteBox.js
+++ src/components/CommentVoteBox.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import styles from './CommentVoteBox.module.css';
+import React from "react";
+import styles from "./CommentVoteBox.module.css";
 
 const CommentVoteBox = (props) => {
-  return <div className={styles.container}>
-    <button>+</button>
-    {props.score}
-    <button>-</button>
-  </div>;
+  return (
+    <div className={styles.container}>
+      <button className={styles.voteButton}>+</button>
+      <span className={styles.scoreNumber}>{props.score}</span>
+      <button className={styles.voteButton}>-</button>
+    </div>
+  );
 };
 
 export default CommentVoteBox;
