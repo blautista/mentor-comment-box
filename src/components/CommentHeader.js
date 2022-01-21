d76ed537c71d720b6bd11cfabad3308277b0dfc3
blautista <bautistaromanazzi@gmail.com>
2022-01-20 21:49:59 -0300

    Prettier indentation

--- src/components/CommentHeader.js
+++ src/components/CommentHeader.js
@@ -1,11 +1,15 @@
 import React from "react";
-import styles from './CommentHeader.module.css';
+import styles from "./CommentHeader.module.css";
 
 const CommentHeader = (props) => {
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
-        <img className={styles.profilePicture} alt="Profile picture" src={props.user.image.png}></img>
+        <img
+          className={styles.profilePicture}
+          alt="Profile picture"
+          src={props.user.image.png}
+        ></img>
         <span className={styles.username}>{props.user.username}</span>
         <span className={styles.createdAt}>{props.createdAt}</span>
       </div>
