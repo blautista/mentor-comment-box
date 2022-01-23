ccf01cc27330e0c499e0cc0e581f4a6c9553544e
blautista <bautistaromanazzi@gmail.com>
2022-01-23 15:04:42 -0300

    Added button icons

--- src/components/CommentHeader.js
+++ src/components/CommentHeader.js
@@ -3,7 +3,6 @@ import styles from "./CommentHeader.module.css";
 import Button from "./Buttons/Button";
 
 const CommentHeader = (props) => {
-  
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
@@ -15,15 +14,30 @@ const CommentHeader = (props) => {
         <span className={styles.username}>{props.user.username}</span>
         <span className={styles.createdAt}>{props.createdAt}</span>
       </div>
-      {props.currentUser.username === props.user.username && 
-      <div>
-      <Button text='Delete' styling='warningNoBorder' onClick={props.onDeleteClick}></Button>
-      <Button text='Edit' styling='successNoBorder' onClick={props.onEditClick}></Button>
-      </div>
-      }
-      {props.currentUser.username !== props.user.username && 
-      <Button text='Reply' styling='successNoBorder' onClick={props.onReplyClick}/>
-      }
+      {props.currentUser.username === props.user.username && (
+        <div>
+          <Button
+            text="Delete"
+            styling="warningNoBorder"
+            icon="./images/icon-delete.svg"
+            onClick={props.onDeleteClick}
+          ></Button>
+          <Button
+            text="Edit"
+            styling="successNoBorder"
+            icon="./images/icon-edit.svg"
+            onClick={props.onEditClick}
+          ></Button>
+        </div>
+      )}
+      {props.currentUser.username !== props.user.username && (
+        <Button
+          icon="./images/icon-reply.svg"
+          text="Reply"
+          styling="successNoBorder"
+          onClick={props.onReplyClick}
+        />
+      )}
     </div>
   );
 };
